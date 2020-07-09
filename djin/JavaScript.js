a9a950ecbdd9b6507b5f607a6fde66650fb4df19
sevenEnot <51885470+sevenEnot@users.noreply.github.com>
2020-07-09 20:49:10 +0300

    1

---
+++ djin/JavaScript.js
@@ -0,0 +1,6 @@
+window.onload = function () {
+    document.getElementById('inter').onclick = function () {
+        document.getElementById('on').checked=true;
+        document.getElementById('otvet').textContent='dsdaasd';
+    }
+}
